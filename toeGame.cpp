f050eaab8d6e61cdec89d2241697624396f75b7e
zarkhodamoradi <zhrkhodmordi@gmail.com>
2022-12-02 18:32:27 +0330

    Add a way to exit the program

--- toeGame.cpp
+++ toeGame.cpp
@@ -4,16 +4,19 @@ using namespace std;
 char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 char turn = 'X';
 bool NoWinner = false;
+int choice;
 //////////////////////////////////////////////////////////////////////////////////////////////
 void displayBoard();
 void playingGmae();
 void Enterchoice(int);
 bool stopGame();
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////
 // main
 int main()
 {
-    while (stopGame())
+    while(stopGame()) 
     {
         displayBoard();
         playingGmae();
@@ -23,6 +26,7 @@ int main()
         cout << "\n\t\tNobody win !!\t\t\n";
     }
 }
+
 // display board function
 void displayBoard()
 {
@@ -38,6 +42,7 @@ void displayBoard()
     cout << "\t"<< " "<< "\t|\t"<< " "<< "\t|\t"<< " " << endl;
     cout << "\t" << board[6] << "\t|\t" << board[7] << "\t|\t" << board[8] << endl;
     cout << "\t"<< " "<< "\t|\t"<< " "<< "\t|\t"<< " " << endl;
+    cout<<"\n Guide {if you want to exit the game , Enter 0} \n";
 }
 // check the chice and put on the boarde
 void Enterchoice(int choice)
@@ -77,7 +82,7 @@ void Enterchoice(int choice)
         break;
     }
 };
-// collect the choise
+// collect the choice
 void playingGmae()
 {
     int choice;
@@ -85,6 +90,8 @@ void playingGmae()
     {
         cout << "\n\t\tPLayer 1 [X] \t\t\n\nEnter your cohice : ";
         cin >> choice;
+
+        
         if (choice >= 1 && choice <= 9)
         {
             if (board[choice - 1] != 'X' && board[choice - 1] != 'O')
@@ -99,7 +106,9 @@ void playingGmae()
                 playingGmae();
             }
         }
-
+else if(choice == 0){
+    exit(0);
+}
         else
         {
             cout << "\n!!!Your choice is not valid!!!\n\nEnter again : \n";
@@ -112,6 +121,8 @@ void playingGmae()
     {
         cout << "\n\t\tPLayer 2 [O]\t\t \n\nEnter your cohice : ";
         cin >> choice;
+
+        
         if (choice >= 1 && choice <= 9)
         {
             if (board[choice - 1] != 'X' && board[choice - 1] != 'O')
@@ -126,7 +137,9 @@ void playingGmae()
                 playingGmae();
             }
         }
-
+else if(choice == 0){
+    exit(0);
+}
         else
         {
             cout << "\n!!!Your choice is not valid!!!\n\nEnter again : \n";
@@ -138,6 +151,7 @@ void playingGmae()
 // equal the member of the boarde array for atop the game
 bool stopGame()
 {
+
 
     if ((board[0] == board[1] && board[0] == board[2]) || (board[3] == board[4] && board[3] == board[5]) || (board[6] == board[7] && board[6] == board[8]) || (board[0] == board[3] && board[0] == board[6]) || (board[1] == board[4] && board[1] == board[7]) || (board[2] == board[5] && board[2] == board[8]) || (board[0] == board[4] && board[0] == board[8]) || (board[2] == board[4] && board[2] == board[6]))
     {
